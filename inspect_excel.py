4c56d748c8a2a093a38e5f4b5bd68f09a0f5fd85
tu_boiler <praikeereekonkayan@gmail.com>
2026-01-24 18:29:01 +0700

    Delete inspect_excel.py

--- inspect_excel.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-try:
-    df = pd.read_excel("%CONDENSATE BOILER.xlsx")
-    print("Columns:", df.columns.tolist())
-    print("First 5 rows:")
-    print(df.head())
-except Exception as e:
-    print(e)
